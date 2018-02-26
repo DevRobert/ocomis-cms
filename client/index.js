941b9c7b46f9e39849b7f53528b3028a5d32a588
Robert Blutner <robert@blutner.de>
2018-02-26 12:24:13 +0100

    React client rendering including hot reloading

---
+++ client/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from '../common/App'
+
+render(
+    <AppContainer><App/></AppContainer>,
+    document.getElementById('root')
+)
+
+console.log('initial rendering!')
+
+module.hot.accept('../common/App', () => {
+    console.log('reload!')
+
+    render(
+        <AppContainer><App/></AppContainer>,
+        document.getElementById('root')
+    )
+})
---
+++ webpack.config.client.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:3001',
+        'webpack/hot/only-dev-server',
+        './client/index'
+    ],
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: 'babel-loader',
+                include: [
+                    path.join(__dirname, 'client'),
+                    path.join(__dirname, 'common')
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
+    devServer: {
+        host: 'localhost',
+        port: 3001,
+        historyApiFallback: true,
+        hot: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    },
+    output: {
+        path: path.join(__dirname, '.build'),
+        publicPath: 'http://localhost:3001/',
+        filename: 'client.bundle.js'
+    }
+}
