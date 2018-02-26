a37bd5a067b0e6935c3636fc76c4cb403c816343
Robert Blutner <robert@blutner.de>
2018-02-26 11:47:49 +0100

    Add React/ server side rendering

---
+++ common/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => <div>Hello from Ocomis CMS!</div>
+
+export default App
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import App from '../common/App'
 
 const app = express()
 
@@ -6,6 +9,25 @@ app.get('/api', (request, response) => {
     response.send({
         message: 'Hello from Node JS!'
     })
+})
+
+app.get('*', (request, response) => {
+    let application = renderToString(<App/>)
+
+    let html = `<!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="x-ua-compatible" content="ie=edge">
+            <title>Ocomis CMS</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        </head>
+        <body>
+            <div id="root">${application}</div>
+        </body>
+    </html>`
+
+    response.send(html)
 })
 
 export default app
